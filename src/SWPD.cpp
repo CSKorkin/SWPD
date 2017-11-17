7eb30562454a584f1fda742127f73377fe2c1cbc
THOMAS.KIESEL <THOMAS.KIESEL@010-172-T2017.school.fortworthisd.net>
2017-11-17 09:04:17 -0600

    Initial commit.

---
+++ src/SWPD.cpp
@@ -0,0 +1,17 @@
+/*
+ * helloworld.cpp
+ *
+ *  Created on: Nov 17, 2017
+ *      Author: THOMAS.KIESEL
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  puts("Hello World!");
+  return EXIT_SUCCESS;
+}
+
+
